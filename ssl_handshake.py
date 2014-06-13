d4756128532b0711ac2cffd034a5b544676e6042
vikas <vikas@netskope.com>
2014-06-13 17:18:16 +0530

    getting cipher suites in preffered order

--- ssl_handshake.py
+++ ssl_handshake.py
@@ -54,6 +54,7 @@ class Connection(object):
     } ClientHello;
 
     """
+#TODO maintain state what all steps are performed
     def client_hello(self, host, port):
         ssl_client_hello = '\x16\x03\x01'
         \x01

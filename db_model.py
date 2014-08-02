d15a0e9ea8a1bf269ec35ca3048a20e65dc5b7b0
Vikas <vikasgupta.nit@gmail.com>
2014-08-02 15:13:08 +0530

    comments added to db_model

--- db_model.py
+++ db_model.py
@@ -1,7 +1,9 @@
 from peewee import *
 import peewee as pw
 
-myDB = pw.MySQLDatabase("mydb", host=" ", port= "", user=" ", password=" ")
+DATABASE = "TLSScanning"
+
+myDB = pw.MySQLDatabase(DATABASE, host="192.168.10.145", port= "3479", user="", password="")
 
 # define a base model calss that specifies which database to use. Then, any subclasses
 # will automatically use the correct DB.
@@ -9,23 +11,26 @@ class BaseModel(Model):
     class Meta:
         database = myDB
 
+# table containing basic information about the scan
 class TLSScan(BaseModel):
-    tls_scan_id = IntegerField(primary_key=True)  # id for this table
-    domain = CharField(null=False)                # domain of the application scanned against
-    app_id = IntegerField(null=False)             # application id from app info db
-    tls_supported = BooleanField(null=False)      # whether the site is tls/ssl enabled
-    cert_number = IntegerField()                  # number of certificates in the chain
-    tls_config_id = ForeignField(TLSConfiguration)# link to TLS configuration table
-    cert_chain_id = ForeignField(CertificateChain)# link the certificate configuration table
+    tls_scan_id = IntegerField(primary_key=True)            # id for this table
+    domain = CharField(null=False)                          # domain of the application scanned against
+    app_id = IntegerField(null=False)                       # application id from app info db
+    tls_supported = BooleanField(null=False)                # whether the site is tls/ssl enabled
+    cert_number = IntegerField()                            # number of certificates in the chain
+    tls_config_id = ForeignField(TLSConfiguration)          # link to TLS configuration table
+    cert_chain_id = ForeignField(CertificateChain)          # link the certificate configuration table
     time_scanned = DateTimeField(default = datetime.datetime.now, null=False) # time of scanning
 
 
+# contains TLS protocol configuration related information
 class TLSConfiguration(BaseModel):
     tls_config_id = IntegerField(primary_key = True)        # id for this table
     protocol_version = CharField()                          # TLS version supported
     ciphersuites = CharField()                              # ciphersuites supported by this website
     compression = CharField()                               # which all compression method are supported.
 
+# contains x509 certificate related information
 class CertificateConfiguration(BaseModel):
     cert_id  = IntegerField(primary_key  = True)            # id for this table
     serial_number = CharField()                             # certificate's serial number
@@ -44,5 +49,3 @@ class CertificateConfiguration(BaseModel):
 class CertificateChain(BaseModel):
     cert_chain_id = IntegerField()                          # id of the certificate chain
     cert_id = ForeignKeyField(CertificateConfiguration)     # certificate id present in this chain
-
-

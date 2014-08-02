b438bc52bb78ba8496f37e32f3c112c5b3b1855a
Vikas <vikasgupta.nit@gmail.com>
2014-08-02 15:52:24 +0530

    use before declaration fixed

--- db_model.py
+++ db_model.py
@@ -11,6 +11,14 @@ class BaseModel(Model):
     class Meta:
         database = myDB
 
+# contains TLS protocol configuration related information
+class TLSConfiguration(BaseModel):
+    tls_config_id = IntegerField(primary_key = True)        # id for this table
+    protocol_version = CharField()                          # TLS version supported
+    ciphersuites = CharField()                              # ciphersuites supported by this website
+    compression = CharField()                               # which all compression method are supported.
+
+
 # table containing basic information about the scan
 class TLSScan(BaseModel):
     tls_scan_id = IntegerField(primary_key=True)            # id for this table
@@ -21,14 +29,6 @@ class TLSScan(BaseModel):
     tls_config_id = ForeignKeyField(TLSConfiguration)          # link to TLS configuration table
     cert_chain_id = ForeignKeyField(CertificateChain)          # link the certificate configuration table
     time_scanned = DateTimeField(default = datetime.datetime.now, null=False) # time of scanning
-
-
-# contains TLS protocol configuration related information
-class TLSConfiguration(BaseModel):
-    tls_config_id = IntegerField(primary_key = True)        # id for this table
-    protocol_version = CharField()                          # TLS version supported
-    ciphersuites = CharField()                              # ciphersuites supported by this website
-    compression = CharField()                               # which all compression method are supported.
 
 # contains x509 certificate related information
 class CertificateConfiguration(BaseModel):
@@ -43,7 +43,6 @@ class CertificateConfiguration(BaseModel):
     key_algorithm = CharField()                             # algorithm of the key
     issuer = CharField()                                    # issuer of the certificate
     sha1_fingerprint = CharField()                          # sha1 fingerprint of the certificate
-    cert_chain_id = ForeignKeyField(CertificateChain)
 
 # many-to-many relationship for certificates contained in the cert chain
 class CertificateChain(BaseModel):

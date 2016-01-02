adeff45af91ab5a5f7bc54698ffbc8fe69af763e
vikas gupta <vikasgupta.nit@gmail.com>
2016-01-02 20:05:47 +0530

    changed naming convention

--- handshake_settings.py
+++ handshake_settings.py
@@ -3,11 +3,11 @@ class HandshakeSettings(object):
         Encapsulates various parameters that can be used with a TLS handshake.
     """
     def __init__(self):
-        self.min_key_size = 1023
-        self.max_key_size = 8193
-        self.cipher_suites = None
-        self.certificate_types = None
-        self.min_version = (3,0)
-        self.max_version = (3,3)
+        self.minKeySize = 1023
+        self.maxKeySize = 8193
+        self.cipherSuites = None
+        self.certificateTypes = None
+        self.minVersion = (3,0)
+        self.maxVersion = (3,3)
 
 

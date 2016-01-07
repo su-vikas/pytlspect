bd4983394ebdea0f0b7f02fa75e38a4efa2bf861
vikas gupta <vikasgupta.nit@gmail.com>
2016-01-07 19:07:02 +0530

    removed tls_scans file and renamed to tlspect, replacing old tlspect

--- tlspect.py
+++ tlspect.py
@@ -1,79 +1,168 @@
-#! /usr/bin/env python
-
-# Author: Vikas Gupta
-# See the LICENSE file for legal information regarding use of this file.
-
+'''
+# =============================================================================
+#      FileName: tlsspect.py
+#          Desc:
+#        Author: Vikas Gupta
+#         Email: vikasgupta.nit@gmail.com
+#      HomePage:
+#       Version: 0.0.1
+#    LastChange: 2016-01-07 18:54:48
+#       History:
+# =============================================================================
+'''
 import sys
 import os
 import copy
 import time
 import csv
-from multiprocessing import Pool
 import argparse
-from result import Result
-from utils.constants import *
 from operator import itemgetter
-from ssl_connection import SSLConnection
-from errors import *
-from cert_checker import CertChecker
 
-class TLSpect:
-    def __init__(self, version = (3,2), port = 443, IP = None, host = None, socketTimeout = 5.0):
-        self.resultObj = Result(host=host, IP=IP)       # object containing result for present scan
-        self.socketTimeout = socketTimeout
-        self.host = host
-        self.port = port
-        self.version = version
-        self.conn = SSLConnection(host = self.host,version= self.version, port = self.port, timeout = self.socketTimeout)
+from handshake_settings import HandshakeSettings
+from ssl_connection import SSLConnection
+from utils.constants import SSLVersions, CipherSuite
+from cert_checker import CertChecker
+from result import Result
+from errors import *
+
+class TLSpect(object):
+    """
+        Encapsulates all the scans that can be performed against a server.
+    """
+
+    def __init__(self, host, port=443):
+        self.settings = HandshakeSettings(host)
+        self.settings.port = port
+        self.result = Result()
+
+        self.isCertificate = False     # to maintain state if certificates has been parsed.
+
+    def startHandshake(self, settings):
+        """
+            Checks if the sslv3 supported by the remote server.
+        """
+        connection = SSLConnection(settings)
+        result = connection.startHandshake(settings)
+        return result
+
+    def _parseExtensions(self, serverHello):
+        extensions = {}
+        if serverHello.next_protos:
+            extensions['next_protocol_negotiation'] = [e for e in serverHello.next_protos]
+        if serverHello.server_name:
+            extensions['server_name'] = True
+        if serverHello.tackExt:
+            extensions['tack'] = True
+        if serverHello.renegotiation_info:
+            extensions['renegotiation_info'] = True
+        if serverHello.heartbeat:
+            extensions['Heartbeat'] = True
+        if serverHello.ocsp:
+            extensions['status_request'] = True
+        if serverHello.session_ticket:
+            extensions['SessionTicket TLS'] = True
+        if serverHello.ec_point_formats:
+            extensions['ec_point_formats'] = True
+
+        return extensions
+
+    def _parseCertificate(self, serverCertificate):
+
+        if not self.isCertificate:
+            checker = CertChecker(self.settings.host, serverCertificate)
+            checker.checkExpiryDate()
+            self.result.certChain = serverCertificate
+
+            self.isCertificate = True
+
+    def getSSLV3Params(self):
+        """
+            Gets all the parameters for SSLv3.
+        """
+        self.settings.version = SSLVersions.SSLV3
+        try:
+            result = self.startHandshake(self.settings)
+            if result:
+                serverHello, serverCertificate = result
+                self.result.isCompressionSSLV3 = serverHello.compression_method
+                if self.settings.version == serverHello.server_version:
+                    self.result.isSSLV3 = True
+
+                self.result.extensionsSSLV3 = self._parseExtensions(serverHello)
+
+                self._parseCertificate(serverCertificate)
+        except TLSRemoteAlert:
+            #TODO add logging.
+            pass
+
+    def getTLSV10Params(self):
+        """
+            Gets all the parameters for SSLv3.
+        """
+        self.settings.version = SSLVersions.TLSV10
+        try:
+            result = self.startHandshake(self.settings)
+            if result:
+                serverHello, serverCertificate = result
+                self.result.isCompressionTLSV10 = serverHello.compression_method
+                if self.settings.version == serverHello.server_version:
+                    self.result.isTLSV10 = True
+
+                self.result.extensionsTLSV10 = self._parseExtensions(serverHello)
+                self._parseCertificate(serverCertificate)
+
+        except TLSRemoteAlert:
+            pass
+
+    def getTLSV11Params(self):
+        """
+        """
+        self.settings.version = SSLVersions.TLSV11
+        try:
+            result = self.startHandshake(self.settings)
+            if result:
+                serverHello, serverCertificate = result
+                self.result.isCompressionTLSV11 = serverHello.compression_method
+                if self.settings.version == serverHello.server_version:
+                    self.result.isTLSV11 = True
+
+                self.result.extensionsTLSV11 = self._parseExtensions(serverHello)
+                self._parseCertificate(serverCertificate)
+        except TLSRemoteAlert:
+            pass
+
+    def getTLSV12Params(self):
+        self.settings.version = SSLVersions.TLSV12
+        try:
+            result = self.startHandshake(self.settings)
+            if result:
+                serverHello, serverCertificate = result
+                self.result.isCompressionTLSV12 = serverHello.compression_method
+                if self.settings.version == serverHello.server_version:
+                    self.result.isTLSV12 = True
+
+                self.result.extensionsTLSV12 = self._parseExtensions(serverHello)
+                self._parseCertificate(serverCertificate)
+        except TLSRemoteAlert:
+            pass
+
+    def getAllParams(self):
+        self.getSSLV3Params()
+        self.getTLSV10Params()
+        self.getTLSV11Params()
+        self.getTLSV12Params()
 
     def getIP(self):
-        """ Get the IP of the host being scanned"""
-        self.resultObj.IP = self.conn.getIP()
-        self.resultObj.printIP()
+        addr = socket.gethostbyname(self.settings.host)
+        self.ip = addr
+        return self.ip
 
-    def enumerateSSLVersion(self):
-        """ Get the list of all SSL versions supported"""
-        sslVersions = self.conn.enumerateSSLVersions()
-        self.resultObj.sslVersions = sslVersions
-        self.resultObj.maxSSLVersion = max(sslVersions, key=itemgetter(1))
-        # self.resultObj.printSSLVersions()
+    def getIPs(self):
+        addr = socket.gethostbyname(self.host)
+        self.ip = addr
+        return self.ip
 
-    def enumerateCiphers(self):
-        """ Get the list of ciphers supported for various versions in default order"""
-        for s in self.resultObj.sslVersions:
-            cipherSuitesDetected = self.conn.enumerateCiphers(s)
-
-            cipherSuitesName = []
-            for cipher_id in cipherSuitesDetected:
-                cipherSuitesName.append(CipherSuite.cipher_suites[cipher_id]['name'])
-            self.resultObj.updateCiphers(s, cipherSuitesName)
-                #print "         " + CipherSuite.cipher_suites[cipher_id]['name']
-
-        self.resultObj.printCipherSuites()
-        print "\n[+] LIST OF POTENTIALLY WEAK CIPHERS:"
-        for cipher_id in cipherSuitesDetected:
-            if 'RC4' in CipherSuite.cipher_suites[cipher_id]['enc']:
-                print "     "+CipherSuite.cipher_suites[cipher_id]['name']
-
-    def isCompression(self):
-        """ Check if the compression is supported """
-        self.resultObj.isCompression = self.conn.isCompressionSupported()
-        self.resultObj.printCompression()
-
-    def certificateTest(self):
-        """ extract the certificate chain information """
-        certificate_chain = self.conn.scanCertificates(self.version)
-        checker = CertChecker(self.host, certificate_chain)
-        checker.checkExpiryDate()
-
-        self.resultObj.certChain = certificate_chain
-        self.resultObj.printCertificates()
-
-
-    def extensionTest(self):
-        self.conn.supportedExtensions()
-
-    def poodleTest(host, version):
+    def poodleSSL(self):
         """
         make a connection with TLS1.0, drop it
         make another connection with SSLv3 and containing TLS_FALLBACK_SCSV ciphersuite in clienthello.
@@ -83,24 +172,17 @@ class TLSpect:
         """
         # make connection with TLS1.0
         try:
-            version = (3,0)
-            conn = SSLConnection(host, version, 443, 5.0)
-            value = conn.doClientHello(host, version)
-            if value is "Alert":
-                print "False"
-            elif value is "Supported":
-                print "True"
+            self.settings.version = SSLVersions.SSLV3
+            self.settings.cipherSuites = CipherSuite.poodleTestSuites
+            result = self.startHandshake(self.settings)
+            self.result.isPoodle = True
 
-            #print "[+] IP:", conn.getIP(), " \n"
-        except Exception, err:
-            print "Error"
+        except TLSRemoteAlert as err:
+            if err.description == 86:
+                self.result.isPoodle = False
 
-        #time.sleep(3)
-        ##make connection with SSLv3.0, i.e emulate downgrade situation with TLS_FALLBACK_SCSV ciphersuite
-        #version = (3,0)
-        #conn = SSLConnection(host, version, 443, 5.0)
-        #conn.doClientHello(host, version)
-        #should give an alert now
+    def poodleTLS(self):
+        pass
 
     def freakTest(self, result):
         """
@@ -176,8 +258,11 @@ class TLSpect:
             results="False"
             return "False"
 
-    def printResults(self):
-        print self.resultObj
+    def openSSLCCS(self):
+        pass
+
+    def heartbleed(self):
+        pass
 
 
 def parse_args():
@@ -186,16 +271,16 @@ def parse_args():
     parser.add_argument("-d", "--domain", required=True, help="The hostname to be scanned for", dest="host")
     parser.add_argument("-p", "--port", help="Port number to scan at, defaults to 443", dest="port")
     parser.add_argument("-a", "--all", help="Scan for all parameters", action="store_true", default=False, dest="all_param_switch")
-    parser.add_argument("-v", "--version", default=(3,2), help="SSL version to scan for", dest="version")
+    # parser.add_argument("-v", "--version", default=(3,2), help="SSL version to scan for", dest="version")
 
-    parser.add_argument("-i","--ips", help="Scan for all IPs", action="store_true", default=False, dest="ips")
-    parser.add_argument("-c","--ciphers", help="Scan only for ciphers supported", action="store_true", default=False,dest="ciphers")
-    parser.add_argument("-z","--compression", help="Scan only for if compression supported", action="store_true", default=False, dest="compress")
-    parser.add_argument("-t","--tls-versions", help="Scan only for supported TLS versions", action="store_true", default=False, dest="tls_versions")
-    parser.add_argument("-w", "--weak-ciphers", help="Report potentially weak ciphers only", action="store_true", default=False, dest="weak_ciphers")
-    parser.add_argument("-C", "--cert", help="Show certificate details", action="store_true", default=False,dest="cert_detail")
-    parser.add_argument("-s", "--cert-chain", help="Show certificate chain details", action="store_true", default=False, dest="cert_chain")
-    parser.add_argument("-e", "--tls-ext", help="Show supported TLS extensions", action="store_true", default=False, dest="tls_ext")
+    # parser.add_argument("-i","--ips", help="Scan for all IPs", action="store_true", default=False, dest="ips")
+    # parser.add_argument("-c","--ciphers", help="Scan only for ciphers supported", action="store_true", default=False,dest="ciphers")
+    # parser.add_argument("-z","--compression", help="Scan only for if compression supported", action="store_true", default=False, dest="compress")
+    # parser.add_argument("-t","--tls-versions", help="Scan only for supported TLS versions", action="store_true", default=False, dest="tls_versions")
+    # parser.add_argument("-w", "--weak-ciphers", help="Report potentially weak ciphers only", action="store_true", default=False, dest="weak_ciphers")
+    # parser.add_argument("-C", "--cert", help="Show certificate details", action="store_true", default=False,dest="cert_detail")
+    # parser.add_argument("-s", "--cert-chain", help="Show certificate chain details", action="store_true", default=False, dest="cert_chain")
+    # parser.add_argument("-e", "--tls-ext", help="Show supported TLS extensions", action="store_true", default=False, dest="tls_ext")
     # Follow rule of having caps for vulnerabilities.
     parser.add_argument("-P", "--poodle", help="Test for POODLE SSL attack", action="store_true", default=False,dest='poodle_switch')
     parser.add_argument("-H", "--heartbleed", help="Test for Heartbled SSL vulnerability", action="store_true", default=False, dest='heartbleed_switch')
@@ -215,14 +300,12 @@ def parse_args():
 
     # CHeck for all TLS Params
     if results.all_param_switch:
+        tlspect.getAllParams()
         tlspect.getIP()
-        tlspect.enumerateSSLVersion()
-        tlspect.enumerateCiphers()
-        tlspect.isCompression()
-        tlspect.certificateTest()
-        tlspect.extensionTest()
+        tlspect.result.output()
 
 
+    """
     # Check for TLS ciphers
     if results.ciphers:
         tlspect.enumerateSSLVersion()
@@ -243,6 +326,7 @@ def parse_args():
     # check for certificate chain
     if results.cert_chain or results.cert_detail:
         tlspect.certificateTest()
+    """
 
     # check for poodle
     if results.poodle_switch:
@@ -259,15 +343,9 @@ def parse_args():
         print tlspect.logjamTest(result)
 
 
-def main(argv):
-    # launchFreak()
+def main():
     parse_args()
 
 if __name__ == "__main__":
-    main(sys.argv)
-
-
-
-
-
+    main()
 
